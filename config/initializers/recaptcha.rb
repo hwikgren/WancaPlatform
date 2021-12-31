2d04b0c21956a19b6fe4b57afa06ab34930636ff
Heidi Jauhiainen <heidi.jauhiainen@helsinki.fi>
2021-12-31 13:33:39 +0200

    Update recaptcha.rb

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = '<YOUR PUBLIC KEY>'
-  config.private_key = '<YOUR PRIVATE KEY>'
+  config.site_key  = '<YOUR PUBLIC KEY>'
+  config.secret_key = '<YOUR PRIVATE KEY>'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
   # Uncomment if you want to use the newer version of the API,
